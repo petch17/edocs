3b35495d671a7e77297aef76f37b66c0385574b6
NAME <psp_phet@hotmail.com>
2019-09-03 15:14:04 +0700

    11111

--- resources/views/inbox/index.blade.php
+++ resources/views/inbox/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('css')
+<link href="./assets/vendors/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('content')
@@ -8,11 +9,6 @@
 <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
     <div class="kt-portlet kt-portlet--mobile">
         <div class="kt-portlet__head kt-portlet__head--lg">
-            <div class="kt-portlet__head-label">
-                <div class="kt-portlet__head-label">
-                    <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="kt_table_1"></label>
-                </div>
-            </div>
             <div class="kt-portlet__head-toolbar">
                 <div class="kt-portlet__head-wrapper">
                     <div class="kt-portlet__head-actions">
@@ -27,7 +23,7 @@
 
         <div class="kt-portlet__body">
             <!--begin: Datatable -->
-            <table class="table table-striped- table-bordered table-hover  ">
+            <table id="table1" class="table table-striped- table-bordered table-hover">
                 <thead>
                     <tr>
 
@@ -60,44 +56,6 @@
 
 
             </table>
-            <!--end: Datatable -->
-            <div class="col-sm-12 col-md-5">
-                <div class="dataTables_info" id="kt_table_1_info" role="status" aria-live="polite">
-                    Showing 1 to 10 of 50 entries
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-md-7">
-                <div class="dataTables_paginate paging_simple_numbers" id="kt_table_1_paginate">
-                    <ul class="pagination">
-                        <li class="paginate_button page-item previous disabled" id="kt_table_1_previous">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="0" tabindex="0" class="page-link">
-                                <i class="la la-angle-left"></i>
-                            </a>
-                        </li>
-                        <li class="paginate_button page-item active">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="1" tabindex="0" class="page-link">1</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="2" tabindex="0" class="page-link">2</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="3" tabindex="0" class="page-link">3</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="4" tabindex="0" class="page-link">4</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="5" tabindex="0" class="page-link">5</a>
-                        </li>
-                        <li class="paginate_button page-item next" id="kt_table_1_next">
-                            <a href="#" aria-controls="kt_table_1" data-dt-idx="6" tabindex="0" class="page-link">
-                                <i class="la la-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
         </div>
     </div>	</div>
 <!-- end:: Content -->
@@ -111,7 +69,11 @@
 
 <script>
 $(document).ready(function () {
-    document.getElementById('inbox').classList.add('active');
+    // document.getElementById('inbox').classList.add('active');
+
+    
+    $('#table1').DataTable();
+
 });
 </script>
 @endsection
