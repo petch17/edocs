9a29730ce7ed650507974f235e3db11830238a71
NAME <psp_phet@hotmail.com>
2019-09-05 10:51:11 +0700

    555

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.homelogin')
 
+@section('css')
+
+@endsection
+
 @section('content')
 <div class="kt-login__container">
-    <div class="kt-login__logo">
+    <div class="kt-login__logo" style="background-color:lightblue">
         <a href="#">
-            <img src="{{asset('assets/media/logos/logo-5.png')}}">
+            {{-- <img src="{{asset('assets/media/logos/logo-5.png')}}"> --}}
+            <img src="{{asset('img/ed2.png')}}" width="100%" height="100%">
         </a>
     </div>
     <div class="kt-login__signin">
@@ -47,14 +52,14 @@
                 </label>
             </div>
 
-            <div class="col kt-align-right">
-                <a href="javascript:;" id="kt_login_forgot" class="kt-login__link">ลืมรหัสผ่าน ?</a>
+            {{-- <div class="col kt-align-right"> --}}
+             {{-- <a href="javascript:;" id="kt_login_forgot" class="kt-login__link">ลืมรหัสผ่าน ?</a> --}}
                 {{-- @if (Route::has('password.request'))
             <a class="btn btn-link" href="{{ route('password.request') }}">
                 {{ __('Forgot Your Password?') }}
                 </a>
                 @endif --}}
-            </div>
+            {{-- </div> --}}
         </div>
         <div class="kt-login__actions">
             <button type="submit" class="btn btn-outline-primary">เข้าสู่ระบบ</button>
@@ -62,7 +67,7 @@
         </div>
         {!! Form::close() !!}
 
-    </div>
+        {{-- </div>
     <div class="kt-login__signup">
         <div class="kt-login__head">
             <h3 class="kt-login__title">เข้าสู่ระบบ</h3>
@@ -125,78 +130,9 @@
         &nbsp;&nbsp;
         <a href="javascript:;" id="kt_login_signup" class="kt-login__account-link"> คลิกเพื่อสร้างบัญชี !</a>
     </div>
-</div>
-@endsection
-
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-<div class="card-body">
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                    value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                    name="password" required autocomplete="current-password">
-
-                @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                        {{ old('remember') ? 'checked' : '' }}>
-
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
-                </button>
-
-                @if (Route::has('password.request'))
-                <a class="btn btn-link" href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a>
-                @endif
-            </div>
-        </div>
-    </form>
-</div>
-</div>
-</div>
-</div>
 </div> --}}
+        @endsection
+
+        @section('js')
+
+        @endsection
