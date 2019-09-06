0ffe8473fb8f815f9cdd76bd882c7b9a75610c58
NAME <psp_phet@hotmail.com>
2019-09-06 19:37:55 +0700

    petch update

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -90,17 +90,17 @@ class InboxController extends Controller
     public function show($id)
     {
         // $data = Edoc::find($id);
-        $data = Edoc::with('tbobjective')->find($id);
-        $data2 = [  'booknum' => $data->booknum, 
-                    'date' => $data->date,
-                    'topic' => $data->topic,
-                    'position' => $data->position,
-                    'name' => $data->tbobjective->name
-                    ];
+        // $data = Edoc::with('tbobjective')->find($id);
+        // $data2 = [  'booknum' => $data->booknum, 
+        //             'date' => $data->date,
+        //             'topic' => $data->topic,
+        //             'position' => $data->position,
+        //             'name' => $data->tbobjective->name
+        //             ];
                     
-        $pdf = PDF::loadView('myPDF', $data2);
+        // $pdf = PDF::loadView('myPDF', $data2);
 
-        return $pdf->stream();
+        // return $pdf->stream();
     }
     public function marksignature($id){
 
