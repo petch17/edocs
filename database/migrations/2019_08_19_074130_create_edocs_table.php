84ff5a2f5e9a136476bde8ca0bbb6ec1f2d80896
Petch17 <php_phet@hotmail.com>
2019-09-16 09:48:20 +0700

    1111

--- database/migrations/2019_08_19_074130_create_edocs_table.php
+++ database/migrations/2019_08_19_074130_create_edocs_table.php
@@ -22,17 +22,19 @@ class CreateEdocsTable extends Migration
 
         Schema::create('edocs', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('topic')->nullable();
-            $table->string('file')->nullable();
-            $table->string('real_filename')->nullable();
-            $table->string('signature')->nullable();
-            $table->integer('width')->nullable();
-            $table->integer('height')->nullable();  
-            $table->integer('getx')->nullable(); 
-            $table->integer('gety')->nullable();  
-            $table->integer('pagelength')->nullable(); 
-            $table->string('approveimage')->nullable(); 
-            $table->string('status')->nullable(); 
+            $table->text('topic')->nullable(); //เรื่อง
+            $table->string('edoc_type')->nullable(); //ประเภทเอกสาร
+            $table->string('file')->nullable(); //ชื่อไฟล์
+            $table->string('real_filename')->nullable(); //ชื่อไฟล์ของจริง
+            $table->string('signature')->nullable(); //ลายเซ็น
+            $table->integer('width')->nullable(); //ความกว้าง
+            $table->integer('height')->nullable();  //ความสูง
+            $table->integer('getx')->nullable(); //ค่า X
+            $table->integer('gety')->nullable();  //ค่า Y
+            $table->integer('pagelength')->nullable();
+            $table->string('approveimage')->nullable();
+            $table->string('status')->nullable(); //สถานะ
+            $table->string('created_by')->nullable(); //สร้างโดยใคร
             // $table->integer('objective_id')->unsigned();
             // $table->foreign('objective_id')->references('id')->on('objective')->onDelete('cascade');
 
